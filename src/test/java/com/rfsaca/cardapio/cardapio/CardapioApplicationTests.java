c9f5ab9062b07387e992af29221f6aaf12843b2e
Rafael Saca <raffaellsaca@gmail.com>
2024-11-12 22:17:46 -0300

    first commit

---
+++ src/test/java/com/rfsaca/cardapio/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfsaca.cardapio.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

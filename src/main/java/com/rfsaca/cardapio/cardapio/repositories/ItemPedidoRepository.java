6349ae5b585611a4f027071d33514ee6191024d6
Rafael Saca <raffaellsaca@gmail.com>
2024-11-14 12:16:19 -0300

    feat: ItemPedido Respository

---
+++ src/main/java/com/rfsaca/cardapio/cardapio/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.rfsaca.cardapio.cardapio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rfsaca.cardapio.cardapio.models.ItemPedido;
+
+public interface ItemPedidoRepository extends  JpaRepository<ItemPedido, Long>{
+
+}

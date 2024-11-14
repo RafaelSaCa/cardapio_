cc12463b2cd9d609ce164e317b62f5523cdcc5fe
Rafael Saca <raffaellsaca@gmail.com>
2024-11-13 22:55:23 -0300

    feat: Exception - recurso não encontrado

---
+++ src/main/java/com/rfsaca/cardapio/cardapio/exceptions/RecursoNotFoundException.java
@@ -0,0 +1,7 @@
+package com.rfsaca.cardapio.cardapio.exceptions;
+
+public class RecursoNotFoundException extends RuntimeException{
+  public RecursoNotFoundException(String message) {
+      super(message);
+  }
+}

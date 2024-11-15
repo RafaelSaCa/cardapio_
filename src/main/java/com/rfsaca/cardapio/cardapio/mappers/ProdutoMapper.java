6ddf505cfb1d8c516784e5087498caabd15a2785
Rafael Saca <raffaellsaca@gmail.com>
2024-11-15 20:07:19 -0300

    refactor: mapper da entidade produto para ProdutoResponse

--- src/main/java/com/rfsaca/cardapio/cardapio/mappers/ProdutoMapper.java
+++ src/main/java/com/rfsaca/cardapio/cardapio/mappers/ProdutoMapper.java
@@ -7,29 +7,34 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import com.rfsaca.cardapio.cardapio.dto.ProdutoDto;
+import com.rfsaca.cardapio.cardapio.dto.response.ProdutoResponse;
 import com.rfsaca.cardapio.cardapio.models.Produto;
 
 @Component
 public class ProdutoMapper {
 
-  private final ModelMapper mapper; 
+    private final ModelMapper mapper;
 
-  public ProdutoMapper(ModelMapper mapper) {
-    this.mapper = mapper;
-  }
+    public ProdutoMapper(ModelMapper mapper) {
+        this.mapper = mapper;
+    }
 
+    public ProdutoDto toDto(Produto produto) {
+        return mapper.map(produto, ProdutoDto.class);
+    }
 
-  public ProdutoDto toDto (Produto produto){
-    return mapper.map(produto, ProdutoDto.class);
-  }
+    public ProdutoResponse toProdutoResponse(Produto produto) {
+        return mapper.map(produto, ProdutoResponse.class);
+    }
 
-  public Produto toEntity (ProdutoDto produtoDto){
-    return mapper.map(produtoDto, Produto.class);
-  }
+    public Produto toEntity(ProdutoDto produtoDto) {
+        return mapper.map(produtoDto, Produto.class);
+    }
 
-  public List<ProdutoDto> toListDto (List<Produto> produtos){
-    return produtos.stream()
-                   .map(produto -> mapper.map(produto, ProdutoDto.class))
-                   .collect(Collectors.toList());
-  }
+    public List<ProdutoDto> toListDto(List<Produto> produtos) {
+        return produtos.stream()
+                .map(produto -> mapper.map(produto, ProdutoDto.class))
+                .collect(Collectors.toList());
+    }
+
 }

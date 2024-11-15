f688f4e4bb7e279196739d870a8f90d8564b0983
Rafael Saca <raffaellsaca@gmail.com>
2024-11-15 20:05:52 -0300

    refactor: JsonIgnore para evitar loop ao deserializar o pedido do itemPedido.

--- src/main/java/com/rfsaca/cardapio/cardapio/models/ItemPedido.java
+++ src/main/java/com/rfsaca/cardapio/cardapio/models/ItemPedido.java
@@ -1,14 +1,23 @@
 package com.rfsaca.cardapio.cardapio.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class ItemPedido {
 
@@ -17,16 +26,16 @@ public class ItemPedido {
   private Long id;
 
   @ManyToOne
-  @JoinColumn(name= "pedido_id")
-  private Pedido pedido;
-
-
-  @ManyToOne
   @JoinColumn(name= "produto_id")
   private Produto produto;
 
   private Integer quantidade;
 
   private Double valor;
+
+  @JsonIgnore
+  @ManyToOne
+  @JoinColumn(name= "pedido_id")
+  private Pedido pedido;
 
 }

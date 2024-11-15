1e7ab90a0c35889c77f8371f1b12d4e3612b75c3
Rafael Saca <raffaellsaca@gmail.com>
2024-11-15 20:00:20 -0300

    refactor: alterando annotations pedido model

--- src/main/java/com/rfsaca/cardapio/cardapio/models/Pedido.java
+++ src/main/java/com/rfsaca/cardapio/cardapio/models/Pedido.java
@@ -2,6 +2,11 @@ package com.rfsaca.cardapio.cardapio.models;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerator;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,9 +15,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Pedido {
 

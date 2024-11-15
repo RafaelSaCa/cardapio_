d8d0b5f4ba9b68a6e715abe2fe6b74d8ea6fe068
Rafael Saca <raffaellsaca@gmail.com>
2024-11-15 20:08:04 -0300

    refactor: findPedidoByUsuario no repository

--- src/main/java/com/rfsaca/cardapio/cardapio/repositories/PedidoRepository.java
+++ src/main/java/com/rfsaca/cardapio/cardapio/repositories/PedidoRepository.java
@@ -1,9 +1,13 @@
 package com.rfsaca.cardapio.cardapio.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rfsaca.cardapio.cardapio.models.Pedido;
+import com.rfsaca.cardapio.cardapio.models.Usuario;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-
+  
+  List<Pedido> findPedidoByUsuario (Usuario usuario);
 }

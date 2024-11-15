733da34d3e15045d0cd8c5dfe9eb2fef73e5f7e7
Rafael Saca <raffaellsaca@gmail.com>
2024-11-15 20:02:13 -0300

    refactor: alterando annotations produto model

--- src/main/java/com/rfsaca/cardapio/cardapio/models/Produto.java
+++ src/main/java/com/rfsaca/cardapio/cardapio/models/Produto.java
@@ -4,9 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Produto {
 
